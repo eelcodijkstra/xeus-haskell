eda5355a80f796995927f7cae785ccab3f7d2a7e
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-11-16 17:41:27 +0900

    Add pixi

--- tasks/bash.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker run -p 8888:8888 --rm -it -v $(pwd):/host xeus-haskell bash
--- tasks/ctest.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker run --rm -i -v "$(pwd):/host" xeus-haskell "ctest --test-dir /tmp --output-on-failure ."
